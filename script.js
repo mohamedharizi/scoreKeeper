7db95db445d4fc96a798b8286216b2a40dd7dcaa
mohamedharizi <mohamed.harizi.dev@gmail.com>
2019-09-28 14:33:32 +0200

    add variables of player

---
+++ script.js
@@ -0,0 +1,16 @@
+
+"use strict";
+
+//Player n°1 
+const btnPlayer1 = document.querySelector("#ctr1");
+const sp1 =  document.querySelector("#score1");
+let score1 = 0;
+
+//Player n°2
+const btnPlayer2 = document.querySelector("#ctr2");
+const sp2 =  document.querySelector("#score2");
+let score2 = 0;
+
+
+
+
